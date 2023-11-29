a289d5d810f1ceafe336e04b6c7f5d9d7b64d354
Rich B <richardbaker1993@gmail.com>
2023-11-29 17:04:44 +0000

    feat: Added button functionality to first question

--- moodflow/components/FlowChartComponents/DayQuestion.tsx
+++ moodflow/components/FlowChartComponents/DayQuestion.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable, Touchable } from 'react-native';
 
 import styles from './styles';
 
 const DayQuestion = () => {
-  const onPress = () => {
-    console.log('Pressed!');
+  const [answer, setAnswer] = useState('');
+
+  const onPress = (value: string) => {
+    setAnswer(value);
+    console.log(answer);
   };
+
   return (
     <View style={styles.container}>
       <Text style={styles.questionText}>How are you feeling today?</Text>
-      <Pressable style={styles.button} onPress={onPress}>
+      <Pressable style={styles.button} onPress={() => onPress('Good')}>
         <Text style={styles.text}>Good</Text>
       </Pressable>
-      <Pressable style={styles.button} onPress={onPress}>
+      <Pressable style={styles.button} onPress={() => onPress('Ok')}>
         <Text style={styles.text}>Ok</Text>
       </Pressable>
-      <Pressable style={styles.button} onPress={onPress}>
+      <Pressable style={styles.button} onPress={() => onPress('Bad')}>
         <Text style={styles.text}>Bad</Text>
       </Pressable>
     </View>
