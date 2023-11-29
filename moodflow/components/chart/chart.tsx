d616811d2d19069ec79992e0573769a28bc9664c
Rich B <richardbaker1993@gmail.com>
2023-11-29 15:41:28 +0000

    feat: generic flowchart screen created

--- moodflow/components/chart/chart.tsx
+++ moodflow/components/chart/chart.tsx
@@ -1,18 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import { SafeAreaView, FlatList, Text, View, Image } from 'react-native';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
-import styles from './chartStyles';
-const logo = require('../../assets/logo.png');
-const bottomLogo = require('../../assets/bottom-logo.png')
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
+import styles from './chartStyles';
+
+const bottomLogo = require('../../assets/bottom-logo.png');
+const logo = require('../../assets/logo.png');
 
 const habitsList: string[] = [
-   'Sleep quality',
-   'Socialising',
-   'Diet',
-   'Staying hydrated',
-   'Exercise'
-  ];
+  'Sleep quality',
+  'Socialising',
+  'Diet',
+  'Staying hydrated',
+  'Exercise',
+];
 
 const Chart = () => {
   let bouncyCheckboxRef: BouncyCheckbox | null = null;
@@ -21,47 +22,50 @@ const Chart = () => {
   // check list state does not persist as not saved in db, but can be done later if needed
   const [checkedItems, setCheckedItems] = useState([]);
 
-  const listItem = ({ item }: {item: string}) => {
+  const listItem = ({ item }: { item: string }) => {
     return (
       <SafeAreaView>
-        <View >
+        <View>
           <BouncyCheckbox
-          style={styles.listItem}
-          textStyle={styles.textStyle}
-          fillColor="white"
-          ref={(ref: any) => (bouncyCheckboxRef = ref)}
-          isChecked={checkboxState}
-          text={item}
-          onPress={() => {
-            setCheckboxState(!checkboxState);
-            setCheckedItems}}
+            style={styles.listItem}
+            textStyle={styles.textStyle}
+            fillColor="white"
+            ref={(ref: any) => (bouncyCheckboxRef = ref)}
+            isChecked={checkboxState}
+            text={item}
+            onPress={() => {
+              setCheckboxState(!checkboxState);
+              setCheckedItems;
+            }}
           />
         </View>
       </SafeAreaView>
-      )
-    }
+    );
+  };
 
   return (
-    <SafeAreaView style= {styles.container}>
+    <SafeAreaView style={styles.container}>
       <View>
-        <Image style={styles.img} source={logo}/>
+        <Image style={styles.img} source={logo} />
       </View>
-      <View  >
-        <Text style={styles.text} >Please let us know at {"\n"}least three habits that {"\n"}affect your mood: </Text>
+      <View>
+        <Text style={styles.text}>
+          Please let us know at {'\n'}least three habits that {'\n'}affect your
+          mood:{' '}
+        </Text>
       </View>
-      <View style = {styles.list}>
+      <View style={styles.list}>
         <FlatList
-            data= {habitsList}
-            keyExtractor={( item ) => item}
-            renderItem={listItem} />
+          data={habitsList}
+          keyExtractor={(item) => item}
+          renderItem={listItem}
+        />
       </View>
       <View>
-        <Image style={styles.bottom} source={bottomLogo}/>
+        <Image style={styles.bottom} source={bottomLogo} />
       </View>
     </SafeAreaView>
-  )
-}
-
-
+  );
+};
 
 export default Chart;
