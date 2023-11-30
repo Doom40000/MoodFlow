043a3726de215fd59a53a91f6ecfe2cfa3085ad5
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-30 14:39:23 +0100

    Add QuoteAPI call to Home

--- moodflow/screens/Home/Home.tsx
+++ moodflow/screens/Home/Home.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, Text, View } from 'react-native';
 
 import styles from './styles';
 import Logo from '../../components/Logo/Logo';
+const Home = () => {
+  const { height, width } = Dimensions.get('window');
+  const [quote, setQuote] = useState('');
 
+  const url = 'https://zenquotes.io/api/quotes';
+
+  // Doesn't work in webview, only on native device (CORS Policy)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(url);
+        const dataInJson = await data.json();
+        setQuote(dataInJson[0].q + '\n-' + dataInJson[0].a);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Logo />
