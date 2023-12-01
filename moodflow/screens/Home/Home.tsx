056dae91749497ee4be73e56a349565c3accf536
Rich B <richardbaker1993@gmail.com>
2023-12-01 09:26:49 +0000

    cleaned up home page

--- moodflow/screens/Home/Home.tsx
+++ moodflow/screens/Home/Home.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+
 import styles from './styles';
 import Logo from '../../components/Logo/Logo';
-import FlowChart from '../FlowChart/FlowChart';
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Logo />
-      <FlowChart/>
     </View>
   );
 };
 
 export default Home;
-
-{/* <View style={styles.quoteContainer}>
-        <View style={styles.quoteHeader}>
-          <Text style={styles.quoteHeaderText}>
-            Here is your quote of the day:
-          </Text>
-        </View>
-        {/* <View style={styles.quote}> */}
-      //   <Text style={styles.quoteText}>
-      //     “It's not about what it is, it's about what it can become.” Dr. Seuss
-      //   </Text>
-      //   {/* </View> */}
-      // </View> */}
