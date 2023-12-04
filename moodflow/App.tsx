45c95ceb435b2d562b67cb52847f20e3feb223de
Rich B <richardbaker1993@gmail.com>
2023-12-04 13:51:30 +0000

    updated links icon.

--- moodflow/App.tsx
+++ moodflow/App.tsx
@@ -1,5 +1,4 @@
 import { Feather, Entypo } from '@expo/vector-icons';
-
 import 'react-native-gesture-handler';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
