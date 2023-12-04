fe453d8c26fec85b0c76e3d3c52bfe0aaa94fcab
dekaratas <-C>
2023-12-04 18:54:21 +0100

    Fix issues with calling backend

--- moodflow/components/pastCharts/Calendar.tsx
+++ moodflow/components/pastCharts/Calendar.tsx
@@ -1,47 +1,65 @@
 import { Moment } from 'moment';
 import React from 'react';
 import { View, ViewStyle } from 'react-native';
-import CalendarPicker, {DateChangedCallback} from 'react-native-calendar-picker';
+import CalendarPicker, {
+  DateChangedCallback,
+} from 'react-native-calendar-picker';
 import DayChart from './DayChart';
 import { receiveNodes } from '../../api/Feedback/feedback_api';
 import styles from './CalendarStyles';
 
 function formatDate(inputDate: Moment): string {
-    const dateObject: Date = inputDate.toDate();
-    const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short', year: 'numeric' };
-    const formattedDate: string = dateObject.toLocaleDateString('en-US', options);
-    const splitDate = formattedDate.replace(/ /g, '').replace(',', '').split('');
-    const day = splitDate.splice(3, 1);
-    const date = splitDate.join('');
-    const finalDate = `${day[0]}${date}`;
-    return finalDate;
+  const dateObject: Date = inputDate.toDate();
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  };
+  const formattedDate: string = dateObject.toLocaleDateString('en-US', options);
+  const splitDate = formattedDate.replace(/ /g, '').replace(',', '').split('');
+  const day = splitDate.splice(3, 1);
+  const date = splitDate.join('');
+  const finalDate = `${day[0]}${date}`;
+  return finalDate;
 }
 
-interface question{
-  question: "question1",
-  answer: "good"
+interface question {
+  question: 'question1';
+  answer: 'good';
 }
 
 const Calendar = () => {
-
-  const onDateChange: DateChangedCallback  = (date) => {
-    const dateString = formatDate(date)
+  const onDateChange: DateChangedCallback = async (date) => {
     // TODO type any to fix here (and add date parameter when controller fixed)
-    const charts = receiveNodes(dateString);
+    // Heyhey :) finalDate currently returns some weird values sometimes
 
-    return <DayChart charts={charts}/>
-  }
+    // const dateString = formatDate(date);
+
+    console.log(formatDate(date));
+    const dateString = '4dec2023';
+    const fetchChart = async () => {
+      const data = await fetch(
+        `http://192.168.188.42:3001/getReq/${dateString}`,
+      );
+      const parsedData = await data.json();
+      console.log('parsed Data: ', parsedData);
+      return parsedData;
+    };
+    const charts = await fetchChart();
+
+    // commented charts out because right now it doesn't throw any errors
+    return <DayChart charts={charts} />;
+  };
 
   return (
     <View style={styles.container}>
       <CalendarPicker
-        selectedDayColor= 'white'
-        todayBackgroundColor= 'pink'
+        selectedDayColor="white"
+        todayBackgroundColor="pink"
         onDateChange={onDateChange}
-
       />
     </View>
   );
-}
+};
 
 export default Calendar;
